a30d3a528dd2a4839619b1ffddfe2c7745e82158
Hall A Online <a-onl@jlab.org>
2021-11-21 09:46:12 -0500

    Setup file for a-onl account added

---
+++ setup_bbcal.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+echo -e "\n"
+echo " ** Welcome to the home of BBCal analysis. Enjoy! ** "
+echo -e "\n"
+
+source $HOME/sbs/sbs_devel/install/bin/sbsenv.sh
+
+#export DATA_DIR=/adaqfs/home/a-onl/sbs/data
+export L_DIR=/adaqfs/home/a-onl/sbs/BBCal_replay/replay/logs
+export DATA_DIR=/adaqeb1/data1
+export OUT_DIR=/adaqfs/home/a-onl/sbs/Rootfiles
+#export DB_DIR=/adaqfs/home/a-onl/sbs/BBCal_replay/git-repo/SBS-replay/DB
+export DB_DIR=/adaqfs/home/a-onl/sbs/sbs_devel/SBS-replay/DB
