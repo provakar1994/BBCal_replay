9a153877b5c1f2f0c9208d845e69f800dbbd0545
Provakar Datta <pdbforce@ifarm1801.jlab.org>
2022-01-15 10:58:51 -0500

    Added HE block eng dist., fixed a few bugs.

--- macros/Beam_analysis/diff_beam_curr_ana.C
+++ macros/Beam_analysis/diff_beam_curr_ana.C
@@ -19,11 +19,16 @@ const int kNcols = 7;
 
 void diff_beam_curr_ana(int nrun=100, int nevent=-1, int fseg=0, int mseg=4 ){
 
+  gErrorIgnoreLevel = kError; // Ignores all ROOT warnings
+
   TChain *C = new TChain("T");
   gmn_tree *T = new gmn_tree(C);
 
-  TString filename = Form("../../Rootfiles/bbshower_gmn_%d_%d"
-			  "_stream0_seg%d_%d.root",nrun,nevent,fseg,mseg);
+  TString filename = Form("/volatile/halla/sbs/datta/GMN_REPLAYS/rootfiles"
+			  "/bbshower_gmn_%d_%d_seg%d_%d.root"
+			  ,nrun,nevent,fseg,mseg);
+  // TString filename = Form("../../Rootfiles/bbshower_gmn_%d_%d"
+  //			  "_stream0_seg%d_%d.root",nrun,nevent,fseg,mseg);
   C->Add(filename);
 
   TString outFile = Form("hist/nclus_nblk_e_%d_%d.root",nrun,nevent);
@@ -34,12 +39,15 @@ void diff_beam_curr_ana(int nrun=100, int nevent=-1, int fseg=0, int mseg=4 ){
   TH2F *h2_nclus_nblk = new TH2F("h2_nclus_nblk","Number of block vs"
 				 " Cluster ID; Cluster ID [0=>Best]; "
 				 "Number of Blocks involved",12,0,12,12,1,13);
-  TH2F *h2_nclus_eng = new TH2F("h2_nclus_eng","Energy of block vs"
+  TH2F *h2_nclus_eng = new TH2F("h2_nclus_eng","Energy of Cluster vs"
 				 " Cluster ID; Cluster ID [0=>Best]; "
-				 "Energy of the block involved",12,0,12,200,0.,4.);
-
+				 "Energy of Cluster",12,0,12,200,0.,4.);
+  TH2F *h2_nclus_seed_eng = new TH2F("h2_nclus_seed_eng","Energy of HE block vs"
+				 " Cluster ID; Cluster ID [0=>Best]; "
+				 "Energy of HE block",12,0,12,200,0.,4.);
   
   Long64_t nevents = C->GetEntries();
+  cout << endl << "Processing " << nevents << " events... " << endl;
 
   // Looping through events
   double progress = 0.;
@@ -48,8 +56,6 @@ void diff_beam_curr_ana(int nrun=100, int nevent=-1, int fseg=0, int mseg=4 ){
     for (int nev = 0; nev < nevents; nev++){ 
  
       T->GetEntry(nev);
-
-      // cout << " nclus= " << T->bb_sh_nclus << endl;
       
       // SH Clustering
       for( int cl=0; cl<(int)T->bb_sh_nclus; cl++ ){
@@ -57,9 +63,11 @@ void diff_beam_curr_ana(int nrun=100, int nevent=-1, int fseg=0, int mseg=4 ){
 	double clus_ID = T->bb_sh_clus_id[cl]; 
 	double num_Block = T->bb_sh_clus_nblk[cl];
 	double clus_eng = T->bb_sh_clus_e[cl];
+	double HEblk_eng = T->bb_sh_clus_eblk[cl];
 
 	h2_nclus_nblk->Fill(cl,num_Block);
 	h2_nclus_eng->Fill(cl,clus_eng);
+	h2_nclus_seed_eng->Fill(cl,HEblk_eng);
 
       }//
 
@@ -77,7 +85,9 @@ void diff_beam_curr_ana(int nrun=100, int nevent=-1, int fseg=0, int mseg=4 ){
       cout.flush();
     }
   }
-  cout << endl << endl;
+  cout << endl << endl << "------" << endl;
+  cout << " Histograms written to: " << outFile << endl;
+  cout << "------" << endl << endl;
 
   fout->Write();
   fout->Close();
