d444f3b619e60abda7f6900bec60ca02e184c47f
Provakar Datta <pdbforce@ifarm1901.jlab.org>
2022-10-25 00:59:00 -0400

    Added fitting

--- macros/Beam_analysis_macros/bbcal_hcal_clust_corr.C
+++ macros/Beam_analysis_macros/bbcal_hcal_clust_corr.C
@@ -1,15 +1,31 @@
+/*
+  This script plots the difference between BBCAL and HCAL trigger times 
+  and fits the peak to get fraction of good events.
+  ------
+  P. Datta Created 10/25/2022 
+*/
+
 #include <iostream>
 
 #include "TH1F.h"
 #include "TChain.h"
 
-void bbcal_hcal_clust_corr(const char* rootfile){
+//gaussian signal peak
+double peak_fit (double *x, double *par) {
+  return par[0]*exp(-0.5*pow((x[0]-par[1])/par[2],2.));
+}
 
-  //cout << "Processing macro.." << endl;
+//poly for background
+double bg_fit (double *x, double *par) {
+  return par[0] + par[1]*x[0];
+}
 
-  //TStopwatch *st = new TStopwatch();
-  //st->Start(kTRUE);
+//global fit function
+double total_fit (double *x, double *par) {
+  return peak_fit(x,&par[0]) + bg_fit(x,&par[3]);
+}
 
+void bbcal_hcal_clust_corr (const char* rootfile) {
   TChain *C = new TChain("T");
   C->Add(rootfile);
 
@@ -18,11 +34,10 @@ void bbcal_hcal_clust_corr(const char* rootfile){
   Double_t bb_sh_nclus = 0., sbs_hcal_nclus = 0.;
   Double_t bb_tdctrig_tdc[6] = {0.}, bb_tdctrig_tdcelemID[6] = {0.};
 
-  TH1F *h_bbcal_hcal_trigtime_diff = new TH1F("h_bbcal_hcal_trigtime_diff","",200,0,1000);
+  TH1F *h_bbcal_hcal_trigtime_diff = new TH1F("h_bbcal_hcal_trigtime_diff","",240,400,640);
   TH2F *h2_bbcal_hcal_corr = new TH2F("h2_bbh_corr","BBCal-HCal Cluster Correlation; BB Shower Rows; HCal Rows",27,1,28,24,1,25);
 
   // Declare trees
-  TTree *T = (TTree*) gDirectory->Get("T");
   C->SetBranchStatus("*",0);
   C->SetBranchStatus("bb.tdctrig.tdc",1);
   C->SetBranchStatus("bb.tdctrig.tdcelemID",1);
@@ -66,13 +81,37 @@ void bbcal_hcal_clust_corr(const char* rootfile){
   c1->cd(1);
   h_bbcal_hcal_trigtime_diff->Draw();
 
+  double par[5], parf[5];
+  TF1* total = new TF1("total", total_fit, 430, 615, 5);
+  total->SetNpx(500);
+
+  // first try
+  total->SetParameters(1,1,1,1,1);
+  h_bbcal_hcal_trigtime_diff->Fit("total", "R0");
+
+  // second try
+  total->SetLineColor(kBlack);
+  total->SetParameters(377,510,15,155,0.06);
+  h_bbcal_hcal_trigtime_diff->Fit("total", "RV+", "ep");
+  total->GetParameters(parf);
+  double totalev = total->Integral(430,615);
+  
+  // plot the signal peak
+  TF1* signal = new TF1("signal", peak_fit, 430, 615, 3);
+  signal->SetNpx(500);
+  signal->SetParameters(parf);
+  signal->SetLineColor(kRed);
+  signal->Draw("same");
+  double coin = signal->Integral(430,615);
+
+  cout << endl << "------" << endl;
+  cout << "No. of Coincidence Events: " << coin << endl;
+  cout << "Total events: " << totalev << endl;
+  cout << "Ratio [Coin/Total]: " << coin/totalev << endl;
+  cout << "------" << endl << endl;
+
   c1->cd(2);
   h2_bbcal_hcal_corr->SetMinimum(-0.1);
   h2_bbcal_hcal_corr->SetStats(0);
   h2_bbcal_hcal_corr->Draw("colz");
-
-  //cout << "Processed macro with " << nevents << " entries." << endl;
-
-  //st->Stop();
-  //cout << "CPU time = " << st->CpuTime() << " s " << " Real time = " << st->RealTime() << " s " << endl;
 }
